63908fc6f06c40d662262fd23ae9b68614768a98
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-10-26 19:11:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,46 @@
 class Translation(object):
-    START_TEXT = """ 𝐇𝐞𝐥𝐥𝐨{},
-𝐈'𝐦 𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭 🥳!
-𝐘𝐨𝐮 𝐜𝐚𝐧 𝐮𝐩𝐥𝐨𝐚𝐝 𝐇𝐓𝐓𝐏/𝐇𝐓𝐓𝐏𝐒 𝐝𝐢𝐫𝐞𝐜𝐭 𝐥𝐢𝐧𝐤, 𝐔𝐬𝐢𝐧𝐠 𝐭𝐡𝐢𝐬 𝐛𝐨𝐭!
+    START_TEXT = """𝐇𝐞𝐥𝐥𝐨! 🙋
 
-/𝐡𝐞𝐥𝐩 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!"""
+𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐅𝐮𝐥 𝐔𝐫𝐥 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐆𝐝𝐫𝐢𝐯𝐞 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫 𝐒𝐢𝐭𝐞𝐬 𝐋𝐢𝐧𝐤... 𝐖𝐢𝐭𝐡 𝐇𝐨𝐭𝐬𝐭𝐚𝐫. 
+
+𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.
+
+/help 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!
+
+𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 :@TheTeleRoid
+"""
+
+    HELP_USER = """𝐈𝐭'𝐬 𝐧𝐨𝐭 𝐭𝐡𝐚𝐭 𝐜𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝 𝐭𝐨 𝐮𝐬𝐞 𝐦𝐞𝐡! 😅
+    
+✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀 𝐓𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐢𝐟 𝐫𝐞𝐪𝐮𝐢𝐫𝐞𝐝. 𝐈𝐭'𝐥𝐥 𝐛𝐞 𝐬𝐚𝐯𝐞𝐝 𝐩𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭𝐥𝐲.💯
+✪ 𝐈𝐟 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐖𝐚𝐬𝐧'𝐭 𝐒𝐞𝐭 𝐁𝐲 𝐘𝐨𝐮, 𝐈𝐭'𝐥𝐥 𝐁𝐞 𝐀𝐮𝐭𝐨 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐅𝐫𝐨𝐦 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞.🥳
+✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀𝐧𝐲 𝐋𝐢𝐧𝐤 𝐓𝐨 𝐁𝐞 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐓𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦.
+✪ 𝐏𝐫𝐞𝐬𝐬 /𝐝𝐞𝐥𝐞𝐭𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐓𝐨 𝐃𝐞𝐥𝐞𝐭𝐞 𝐘𝐨𝐮𝐫 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
+✪ 𝐒𝐞𝐥𝐞𝐜𝐭 𝐭𝐡𝐞 𝐛𝐮𝐭𝐭𝐨𝐧.
+   𝐒𝐕𝐢𝐝𝐞𝐨 - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
+   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
+   𝐕𝐢𝐝𝐞𝐨  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
+   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
+𝐍𝐁 : 𝐈𝐭 𝐢𝐬 𝐑𝐞𝐜𝐨𝐦𝐦𝐞𝐧𝐝𝐞𝐝 𝐓𝐨 𝐔𝐬𝐞 𝐀 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐁𝐞𝐜𝐚𝐮𝐬𝐞, 𝐒𝐨𝐦𝐞 𝐓𝐢𝐦𝐞 𝐖𝐨𝐧'𝐭 𝐔𝐩𝐥𝐨𝐚𝐝 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞 𝐖𝐢𝐭𝐡𝐨𝐮𝐭 𝐚 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
+𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩 : @TeleRoid14
+"""
+    ABOUT_TEXT = """<b>🤖 𝐌𝐲 𝐍𝐚𝐦𝐞 : <a href='https://t.me/TeleRoid_Hotstar_Bot'> @𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝_𝐇𝐨𝐭𝐬𝐭𝐚𝐫_𝐁𝐨𝐭 </a></b>
+
+<b>👨‍💻 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : <a href='https://t.me/PredatorHackerzZ'> @𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫𝐇𝐚𝐜𝐤𝐞𝐫𝐳𝐙 </a></b>
+
+<b>📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : 𝐏𝐲𝐭𝐡𝐨𝐧𝟑</b>
+
+<b>🌐 𝐒𝐞𝐫𝐯𝐞𝐫 : <a href='https://www.heroku.com'> 𝐇𝐞𝐫𝐨𝐤𝐮 </a></b>
+
+<b>📔 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : Pyrogram 1.0.7</b>
+
+<b>👥 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞 : <a href='https://github.com/P-Phreak/URLuploader-With-Hotstar'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a></b>
+
+<b>📢 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩 : <a href='https://t.me/TeleRoid14'>@𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a></b>
+
+<b>🌀 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 : <a href='https://t.me/TeleRoidGroup'>@𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝𝐆𝐫𝐨𝐮𝐩</a></b>
+
+<b>👨‍🔧 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐁𝐨𝐭𝐥𝐢𝐬𝐭 : <a href='https://t.me/TGRobot_List'>@𝐓𝐆𝐑𝐨𝐛𝐨𝐭_𝐋𝐢𝐬𝐭</a></b>"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
@@ -16,17 +53,6 @@ URL | filename | username | password"""
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ 𝐂𝐮𝐬𝐭𝐨𝐦 𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐜𝐥𝐞𝐚𝐫𝐞𝐝 𝐬𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲."
     CUSTOM_CAPTION_UL_FILE = "{}"
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-    HELP_USER = """𝐇𝐨𝐰 𝐭𝐨 𝐔𝐬𝐞 𝐌𝐞? 𝐅𝐨𝐥𝐥𝐨𝐰 𝐓𝐡𝐞𝐬𝐞 𝐬𝐭𝐞𝐩𝐬!
-    
-𝟏. 𝐒𝐞𝐧𝐝 𝐮𝐫𝐥 (𝐞𝐱𝐚𝐦𝐩𝐥𝐞.𝐝𝐨𝐦𝐚𝐢𝐧/𝐅𝐢𝐥𝐞.𝐦𝐩𝟒 | 𝐍𝐞𝐰 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞.𝐦𝐩𝟒).
-𝟐. 𝐒𝐞𝐧𝐝 𝐈𝐦𝐚𝐠𝐞 𝐀𝐬 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 (𝐎𝐩𝐭𝐢𝐨𝐧𝐚𝐥).
-𝟑. 𝐒𝐞𝐥𝐞𝐜𝐭 𝐭𝐡𝐞 𝐛𝐮𝐭𝐭𝐨𝐧.
-   𝐒𝐕𝐢𝐝𝐞𝐨 - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 (𝐯𝐢𝐝𝐞𝐨) 𝐚𝐬 𝐟𝐢𝐥𝐞 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐕𝐢𝐝𝐞𝐨  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐅𝐢𝐥𝐞   - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-
-𝐈𝐟 𝐛𝐨𝐭 𝐝𝐢𝐝𝐧'𝐭 𝐫𝐞𝐬𝐩𝐨𝐧𝐝, 𝐜𝐨𝐧𝐭𝐚𝐜𝐭 @TeleRoid14"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
