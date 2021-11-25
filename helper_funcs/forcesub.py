7cdca0d4aa6f2cba555fee06ed53f31c9b00ad01
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-11-25 19:36:48 +0530

    Update forcesub.py

--- helper_funcs/forcesub.py
+++ helper_funcs/forcesub.py
@@ -20,7 +20,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\nAnd Still If Bot AskS For Joining Updates Channel then Join @MoviesFlixers_DL.",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
