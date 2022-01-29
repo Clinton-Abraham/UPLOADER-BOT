f83af366da875f619259a3764c8a8851977925b3
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-01-29 09:46:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -9,14 +9,8 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 import os
-import sqlite3
 
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
+from config import Config
 # the Strings used for this "thing"
 from translation import Translation
 
