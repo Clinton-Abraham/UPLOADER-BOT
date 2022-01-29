966bc43d216022131dcf2606a4f556308da10893
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-01-29 09:45:13 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -13,11 +13,8 @@ import numpy
 import os
 from PIL import Image
 import time
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+
+from config import Config
 # the Strings used for this "thing"
 from translation import Translation
 from pyrogram import Client as Clinton
