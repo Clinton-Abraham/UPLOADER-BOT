24ee1f0ba9c9a26c771ed410059fd864af97026c
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-01-29 09:19:02 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,8 +7,6 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
-    # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -16,9 +14,8 @@ class Config(object):
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    CHUNK_SIZE = int(128)
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = None #os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -26,12 +23,9 @@ class Config(object):
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
     # your telegram id
     OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     MAX_RESULTS = "50"
