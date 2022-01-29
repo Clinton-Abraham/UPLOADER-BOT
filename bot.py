07dcd15d612715a0f7c5eacc3ef4b838cf19a99b
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-01-29 09:35:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ else:
     from config import Config
 
 from pyrogram import Client as Clinton
-from pyrogram import filters
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
@@ -26,10 +25,9 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton(
-        "@UPLOADER_X_BOT",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
+    Warrior = Clinton("@UPLOADER_X_BOT",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins)
     Warrior.run()
