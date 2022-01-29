38e499164713aefd3c9d8a120c6344242e015c53
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-01-29 09:41:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,9 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
 
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
-
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
+from config import Config
 from pyrogram import Client as Clinton
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -26,8 +12,8 @@ if __name__ == "__main__" :
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
     Warrior = Clinton("@UPLOADER_X_BOT",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins)
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    plugins=plugins)
     Warrior.run()
