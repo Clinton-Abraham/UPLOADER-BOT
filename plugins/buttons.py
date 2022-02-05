0a51142377b3d517c6fee0f7fface8d80529bfc2
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-02-05 13:45:13 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -6,11 +6,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 class Button(object):
 
       BUTTONS01 = InlineKeyboardMarkup( [ [ InlineKeyboardButton(text="📁 YTS", callback_data='00'),
-                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="!1 ") ],
+                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="1 ") ],
                                           [ InlineKeyboardButton(text="📁 Anime", callback_data='00'),
-                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="!2 ") ],
+                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="2 ") ],
                                           [ InlineKeyboardButton(text="📁 1337x", callback_data='00'),
-                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="!3 " ) ],
+                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="3 " ) ],
                                           [ InlineKeyboardButton(text="📁 ThePirateBay", callback_data='00'),
-                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="!4 ") ],
+                                            InlineKeyboardButton(text="🔍 ꜱᴇᴀʀᴄʜ", switch_inline_query_current_chat="4 ") ],
                                           [ InlineKeyboardButton(text="❌", callback_data="X0") ] ] )
