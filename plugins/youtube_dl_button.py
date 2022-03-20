d8457cb5b9d64da4c9e8b19ad61a99005e6777d1
cpanel10x <vutn0911@gmail.com>
2022-03-20 15:34:46 +0700

    Update youtube_dl_button.py
    Fix file name so long (use facebook post as file name)

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -69,7 +69,7 @@ async def youtube_dl_call_back(bot, update):
         if youtube_dl_url is not None:
             youtube_dl_url = youtube_dl_url.strip()
         if custom_file_name is not None:
-            custom_file_name = custom_file_name.strip()
+            custom_file_name = custom_file_name.strip()[:50]
         # https://stackoverflow.com/a/761825/4723940
         if youtube_dl_username is not None:
             youtube_dl_username = youtube_dl_username.strip()
