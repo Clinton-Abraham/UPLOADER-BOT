72422d5b8a32197cbf8deb4c435b5a9489cc8879
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-07-15 01:48:00 +0530

    Upload limit updated 2.00 GB to 4.00 GB ✅

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 4194304000 #2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(128)
