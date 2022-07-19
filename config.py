c349d30f32d3c0e5bd3d36061d3e8dd91e0d888e
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-07-20 03:39:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,3 +29,4 @@ class Config(object):
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     MAX_RESULTS = "50"
+    PREMIUM_USER = os.environ.get("PREMIUM_USER")
