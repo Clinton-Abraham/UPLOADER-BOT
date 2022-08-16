3408a069ec64664c756c63705bcc85ecf1c5038e
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-08-17 03:49:23 +0530

    @BOT_X_BOT..........

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton("@UPLOADER_X_BOT",
+    Warrior = Clinton("@BOT_X_BOT",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
